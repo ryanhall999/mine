dc26efb9bd0c8ece2121c6473a2781c4b332ebcd
Ryan Hall <ryanhall999@gmail.com>
2020-12-11 12:47:05 -0500

    logic

--- mine.py
+++ mine.py
@@ -15,22 +15,28 @@ def minesweeper (n, k) :
       arr[y][x-1] += 1 # center left
 
     if(x >= 1 and x <= 4) and (y >= 1 and y <= 4):
-      arr[y-1][x-1] += 1 # top left
+      if arr[y-1][x-1] != 'X':
+        arr[y-1][x-1] += 1 # top left
 
     if(x >= 0 and x <= 3) and (y >= 1 and y <= 4):
-      arr[y-1][x+1] += 1 # top right
+      if arr[y-1][x+1] != 'X':
+        arr[y-1][x+1] += 1 # top right
 
     if(x >= 0 and x <= 4) and (y >= 1 and y <= 4):
-      arr[y-1][x] += 1 # top center 
+      if arr[y-1][x] != 'X':
+        arr[y-1][x] += 1 # top center 
 
     if(x >= 0 and x <= 3) and (y >= 0 and y <= 3):
-      arr[y+1][x+1] += 1 # bottom right   
+      if arr[y+1][x+1] != 'X':
+        arr[y+1][x+1] += 1 # bottom right   
 
     if(x >= 1 and x <= 4) and (y >= 0 and y <= 3):
-      arr[y+1][x-1] += 1 # bottom left   
+      if arr[y+1][x-1] != 'X':
+        arr[y+1][x-1] += 1 # bottom left   
 
     if(x >= 0 and x<= 4) and (y >= 0 and y <= 3):
-      arr[y+1][x] += 1 # bottom center   
+      if arr[y+1][x] != 'X':
+        arr[y+1][x] += 1 # bottom center   
 
   for row in arr:
     print(" ".join(str(cell) for cell in row))
