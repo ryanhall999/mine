a376b51601698aa702660c7255e8cbc0b9326c1c
Ryan Hall <ryanhall999@gmail.com>
2020-12-11 12:32:42 -0500

    logic

--- mine.py
+++ mine.py
@@ -2,37 +2,38 @@ import random
 
 def minesweeper (n) :
   arr = [[0 for row in range (n)] for column in range (n)]
-  x = random.randint(0,4)
-  y = random.randint(0,4)
-  arr[y][x] = 'X' 
+  for num in range(2):
+    x = random.randint(0,4)
+    y = random.randint(0,4)
+    arr[y][x] = 'X' 
 
-  if (x >= 1 and x <= 3):
-    arr[y][x+1] += 1 # center right
-    arr[y][x-1] += 1 # center left
+    if (x >= 1 and x <= 3):
+      arr[y][x+1] += 1 # center right
+      arr[y][x-1] += 1 # center left
 
-  if(x == 0):
-    arr[y][x+1] += 1 # center right
-  
-  if(x == 4):
-    arr[y][x-1] += 1 # center left
+    if(x == 0):
+      arr[y][x+1] += 1 # center right
+    
+    if(x == 4):
+      arr[y][x-1] += 1 # center left
 
-  if(x >= 1 and x<= 4) and (y >= 1 and y <= 4):
-    arr[y-1][x-1] += 1 # top left
+    if(x >= 1 and x<= 4) and (y >= 1 and y <= 4):
+      arr[y-1][x-1] += 1 # top left
 
-  if(x >= 0 and x<= 3) and (y >= 1 and y <= 4):
-    arr[y-1][x+1] += 1 # top right
+    if(x >= 0 and x<= 3) and (y >= 1 and y <= 4):
+      arr[y-1][x+1] += 1 # top right
 
-  if(x >= 0 and x<= 4) and (y >= 1 and y <= 4):
-    arr[y-1][x] += 1 # top center 
+    if(x >= 0 and x<= 4) and (y >= 1 and y <= 4):
+      arr[y-1][x] += 1 # top center 
 
-  if(x >= 0 and x<= 3) and (y >= 0 and y <= 3):
-    arr[y+1][x+1] += 1 # bottom right   
+    if(x >= 0 and x<= 3) and (y >= 0 and y <= 3):
+      arr[y+1][x+1] += 1 # bottom right   
 
-  if(x >= 1 and x<= 4) and (y >= 0 and y <= 3):
-    arr[y+1][x-1] += 1 # bottom left   
+    if(x >= 1 and x<= 4) and (y >= 0 and y <= 3):
+      arr[y+1][x-1] += 1 # bottom left   
 
-  if(x >= 0 and x<= 4) and (y >= 0 and y <= 3):
-    arr[y+1][x] += 1 # bottom center   
+    if(x >= 0 and x<= 4) and (y >= 0 and y <= 3):
+      arr[y+1][x] += 1 # bottom center   
 
   for row in arr:
     print(" ".join(str(cell) for cell in row))
